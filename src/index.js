7c19bd8bc6424ce4c4558b4a48d66d85f0055968
Olga Kolesova <olka.kolesova@gmail.com>
2021-11-23 20:58:07 +0000

    using function inside jsx

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click on me!';
+}
+
 const App = () => {
   return (
     <div>
-      <label class="label" htmlFor="">Enter name:</label>
+      <label className="label" htmlFor="">Enter name:</label>
       <input id="name" type="text" />
-      <button style={{backgroundColor: "blue", color: "white"}}>Submit</button>
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {getButtonText()}
+      </button>
     </div>
   );
 };
